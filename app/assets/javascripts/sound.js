7cfa8093ea75467f3dde9a6b8bafa188516f4b3e
kuyenda <cocoon.co.up@gmail.com>
2019-11-02 15:14:32 +0800

    update css

--- app/assets/javascripts/sound.js
+++ app/assets/javascripts/sound.js
@@ -9,7 +9,10 @@
 // }
 
 let soundID = document.getElementById("player");
-let playlist = ['Go Shina.ts']
+let playlist = [
+	'Diviners - Thousand Years (ft. Patrick Baker) by Diviners - Free Listening on SoundCloud.ts',
+	'James Carter - Give Me Your Love (Diviners Remix) by Diviners.ts'
+];
 
 function getRandomInt(max) {
 	return Math.floor(Math.random() * Math.floor(max));
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   def home
     render 'layouts/home.html.erb'
   end
+  def index
+    render 'layouts/index'
+  end
 end
