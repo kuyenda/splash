93a2f210d5de9f024d643db44b705c2bf6ce7976
kuyenda <cocoon.co.up@gmail.com>
2019-12-06 15:55:03 +0800

    deploy gem

--- app/helpers/examples_helper.rb
+++ app/helpers/examples_helper.rb
@@ -1,6 +1,6 @@
 module ExamplesHelper
   def rand_hex_color
-    '#' << SecureRandom.hex(3)
+    "#" << SecureRandom.hex(1) << "1632"
   end
   def reverse_hex_color(color)
     rgb = color[1..-1].hex
