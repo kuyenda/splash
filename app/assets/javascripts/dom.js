ac2713a2aa6a63993b212c4d6a8a10aa42fb7923
kuyenda <cocoon.co.up@gmail.com>
2019-12-10 20:40:09 +0800

    fix deploy bug

--- app/assets/javascripts/dom.js
+++ app/assets/javascripts/dom.js
@@ -27,18 +27,3 @@ if (!window.et_player) {
 		},
 	}
 }
-$(document).on('turbolinks:load', function() {
-	// Prism.highlightAll();
-});
-// new Chartist.Line('.ct-chart', {
-// 	labels: ['Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'],
-// 	series: [
-// 		[1, 1, 1, 2, 1, 1, 1]
-// 	]
-// }, {
-// 	high: 5,
-// 	low: 0,
-// 	onlyInteger: true,
-// 	showArea: true,
-// 	ticks: ['One', 'Two', 'Three'],
-// });
