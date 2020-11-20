39eeceac5d51becf42306b278404b0621e78adb9
kuyenda <cocoon.co.up@gmail.com>
2020-11-20 14:11:31 +0800

    rewrire layouts

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,19 +1,22 @@
 class DocumentsController < ApplicationController
   DOC_VERSION = "1.00"
-  DOC_META = [
-    ["naming", "question", "命名规则"],
-    ["typography", "type", "段落"],
-    ["color", "droplet-half", "颜色"],
-    ["border", "app", "边框"],
-    ["layout", "layers-half", "图层"],
-    ["icon", "bootstrap-fill", "图标"],
-    ["debugging", "terminal-fill", "调试"],
-  ]
+  DOC_MAP = {
+    "总览": [
+      { href: "naming", text: "命名规则", icon: "question" },
+      { href: "selector", text: "选择器的区别", icon: nil },
+    ],
+    "其他": [
+      { href: "typography", text: "段落", icon: "type" },
+      { href: "color", text: "颜色", icon: "droplet-half" },
+      { href: "border", text: "边框", icon: "app" },
+      { href: "layout", text: "图层", icon: "layers-half" },
+      { href: "icon", text: "图标", icon: "bootstrap-fill" },
+      { href: "debug", text: "调试", icon: "terminal-fill" },
+    ]
+  }
+
   def show
     @id = params[:id]
-    @docs ||= []
-    DOC_META.each do |item|
-      @docs.push({ href: item[0], icon_name: item[1], text: item[2] })
-    end
+    @docs ||= DOC_MAP.values.flatten
   end
 end
